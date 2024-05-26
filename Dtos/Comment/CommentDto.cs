25c4bde832ad1d7b70eb67c7bbc1a66a67db7a2d
Bach Linh <nhatlinh.bach1997no1@gmail.com>
2024-05-26 23:03:10 +0700

    add files

--- Dtos/Comment/CommentDto.cs
+++ Dtos/Comment/CommentDto.cs
@@ -1,19 +1,17 @@
 using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace api.Models
+namespace api.Dtos.Comment
 {
-    [Table("Comments")]
-    public class Comment
+    public class CommentDto
     {
         public int Id { get; set; }
         public string Title { get; set; } = string.Empty;
         public string Content { get; set; } = string.Empty;
         public DateTime CreatedOn { get; set; } = DateTime.Now;
+        public string CreatedBy { get; set; } = string.Empty;
         public int? StockId { get; set; }
-        public Stock? Stock { get; set; }
     }
 }
